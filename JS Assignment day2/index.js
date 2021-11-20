513beaba6d6ff314b311c94f5f6d78e66ff75d1e
Codingshagy <88201408+Codingshagy@users.noreply.github.com>
2021-11-20 16:07:28 +0530

    Add files via upload

---
+++ JS Assignment day2/index.js
@@ -0,0 +1,10 @@
+let a = 100
+let b = 200
+let c = 300
+
+if (a>=b && a>=c)
+console.log(`${a} is the biggest`)
+else if (b>=a && b>=c)
+console.log(`${b} is the biggest`)
+else (c>=a && c>=b)
+console.log(`${c} is the biggest`)
